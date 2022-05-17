287ee3e5a8d9beabf280f815aa3ef9fc1fa2b68d
Vlad <vladkalyuzhnyu@gmail.com>
2022-05-17 08:26:32 +0300

    web gradle

---
+++ web/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose") version "1.0.0"
+}
+
+group = "androidovshchik"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(project(":common"))
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
